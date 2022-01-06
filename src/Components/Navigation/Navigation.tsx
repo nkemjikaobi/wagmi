c6bdf097edbb63860e2f343b4e9f48c52ace9ad1
Obi NkemjIka Derick <nkemjikaobi@gmail.com>
2022-01-06 10:24:20 +0100

    Deploy staging

--- src/Components/Navigation/Navigation.tsx
+++ src/Components/Navigation/Navigation.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import logo from '../../Assets/images/png/logo.jpeg';
 
 const Navigation = () => {
-    return (
-        <div>
-            Nav
-        </div>
-    )
-}
+	return (
+		<div>
+			<div>
+                <img src={logo} width={50} height={50}/>
+			</div>
+			<div></div>
+		</div>
+	);
+};
 
-export default Navigation
+export default Navigation;
