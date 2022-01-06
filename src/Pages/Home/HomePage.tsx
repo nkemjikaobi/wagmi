4387ee62d5d3d6f35fc3d8392c67760c67fcbdf4
Obi NkemjIka Derick <nkemjikaobi@gmail.com>
2022-01-06 16:09:46 +0100

    Header section complete

--- src/Pages/Home/HomePage.tsx
+++ src/Pages/Home/HomePage.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 import BasePageLayout from '../../Components/BasePageLayout/BasePageLayout';
+import styles from "./HomePage.module.scss";
 const HomePage = () => {
-	return <BasePageLayout>home</BasePageLayout>;
+	return (
+		<BasePageLayout>
+			<div className={styles.wrapper}>
+				<h1>
+					Invest In The Future<span>Of Currency</span>{' '}
+				</h1>
+				<p>
+					We offers users a fully operational long-term rental platform. It
+					plans to leverages blockchain<br /> technology to ensure seamless rental
+					experience.
+				</p>
+				<div className={styles.buttonWrapper}>
+					<button>Crypto Consultation</button>
+					<button>Learn More</button>
+				</div>
+			</div>
+		</BasePageLayout>
+	);
 };
 
 export default HomePage;
